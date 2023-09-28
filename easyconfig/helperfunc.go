fff470e9c2fcf6ba29e470bb624f2035f345d2dd
feng.dev <feng@vitruvio.cl>
2023-09-28 12:57:22 -0300

    Update helperfunc.go

--- easyconfig/helperfunc.go
+++ easyconfig/helperfunc.go
@@ -6,15 +6,13 @@ import (
 )
 
 func GetConfig(path string) (map[string]interface{}, error) {
-	file, err := os.Open(path)
+	file, err := ReadFromFile(path)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
-	var data map[string]interface{}
 
-	err = json.NewDecoder(file).Decode(&data)
+	data, err := CreateMapFromJsonData(file)
 	if err != nil {
 		return nil, err
 	}
@@ -26,4 +24,38 @@ func IsValidPath(path string) bool {
 	_, err := os.Stat(path)
 	
 	return err== nil
+}
+
+
+func CreateJsonDataFromMap(data map[string]interface{}) ([]byte, error) {
+	return json.Marshal(data)
+}
+
+func CreateMapFromJsonData(jsondata []byte) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	err := json.Unmarshal(jsondata, &result)
+	return result, err
+}
+
+
+func WriteToFile(path string, data []byte) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(data)
+	return err
+}
+
+func ReadFromFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	
+	var data []byte
+	_, err = file.Read(data)
+	return data, err
 }
